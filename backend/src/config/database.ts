fdae1748c6595a3b5f1ab1f2b587b2f9ae17ca23
caiomartinni <caio.martini@grupointegrado.br>
2025-06-23 19:23:16 -0300

    feat(docker): implementando Docker

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,8 +1,31 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const sequelize = new Sequelize("cartalogo", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+dotenv.config();
+
+const isTest = process.env.NODE_ENV === "test";
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME!,
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD!,
+  {
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT) || 3306,
+    dialect: "mysql",
+    logging: !isTest,
+  }
+);
+
+if (!isTest) {
+  (async () => {
+    try {
+      await sequelize.sync({ alter: true });
+      console.log("Banco de dados sincronizado.");
+    } catch (error) {
+      console.error("Erro ao sincronizar o banco de dados:", error);
+    }
+  })();
+}
 
 export default sequelize;
--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,14 +1,12 @@
 import jwt from "jsonwebtoken";
 import UserModel from "../models/UserModel";
 
-const SECRET = "seuSegredoSuperSeguro"; // Mude isso para uma variável de ambiente!
+const SECRET = "seuSegredoSuperSeguro";
 
 export const generateToken = (user: UserModel) => {
-  return jwt.sign(
-    { id: user.id, email: user.email }, // Dados no token
-    SECRET,
-    { expiresIn: "1h" } // Token expira em 1 hora
-  );
+  return jwt.sign({ id: user.id, email: user.email }, SECRET, {
+    expiresIn: "1h",
+  });
 };
 
 export const verifyToken = (token: string) => {
